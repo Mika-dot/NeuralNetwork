47f07096ca69abdb0ff5053eab2e81f0c3fdcc40
Serebrovsky Sergey Maksimovich <61110689+Mika-dot@users.noreply.github.com>
2023-10-24 12:45:33 +0300

    Add files via upload

---
+++ Et_example_Console/Program.cs
@@ -0,0 +1,25 @@
+﻿using Et_example_Console;
+
+Et_example.ModelInput sampleData = new Et_example.ModelInput()
+{
+    Date = @"2012-01-02",
+    Precipitation = 10.9F,
+    Temp_max = 10.6F,
+    Temp_min = 2.8F,
+    Wind = 4.5F,
+};
+
+var predictionResult = Et_example.Predict(sampleData);
+
+
+Console.WriteLine($"Date: {@"2012-01-02"}");
+Console.WriteLine($"Precipitation: {10.9F}");
+Console.WriteLine($"Temp_max: {10.6F}");
+Console.WriteLine($"Temp_min: {2.8F}");
+Console.WriteLine($"Wind: {4.5F}");
+Console.WriteLine($"Weather: {@"rain"}");
+
+
+Console.WriteLine($"\n\nPredicted Weather: {predictionResult.PredictedLabel}\n\n");
+
+
