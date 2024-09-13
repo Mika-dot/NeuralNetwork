94262594f3c744e4b500f75dc60cdfac64d9f03d
Serebrovsky Sergey Maksimovich. <61110689+Mika-dot@users.noreply.github.com>
2024-09-13 11:29:45 +0300

    Add files via upload
    Файлы

---
+++ WindowsFormsautoencoder/HeatMap/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace HeatMap
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button2);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.pictureBox1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.pictureBox2);
+            this.splitContainer1.Size = new System.Drawing.Size(800, 450);
+            this.splitContainer1.SplitterDistance = 389;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(389, 450);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(407, 450);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox2.TabIndex = 0;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(3, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(58, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "<";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(67, 3);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(56, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(3, 32);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(120, 23);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Файлы";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.splitContainer1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ WindowsFormsautoencoder/HeatMap/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using AI;
+using AI.DataStructs.Algebraic;
+using AI.ML.Classifiers;
+using AI.ML.NeuralNetwork.CoreNNW;
+using AI.ML.NeuralNetwork.CoreNNW.Activations;
+using AI.ML.NeuralNetwork.CoreNNW.Datasets;
+using AI.ML.NeuralNetwork.CoreNNW.Layers;
+using AI.ML.NeuralNetwork.CoreNNW.Loss;
+using AI.ML.NeuralNetwork.CoreNNW.Optimizers;
+using AI.ML.NeuralNetwork.CoreNNW.Train;
+using AI.ML.SeqPredict;
+using AI.ML.Datasets;
+using static System.Resources.ResXFileRef;
+using System.Drawing.Drawing2D;
+using System.Security.Policy;
+using AI.ComputerVision;
+using Matrix = AI.DataStructs.Algebraic.Matrix;
+using static System.Net.Mime.MediaTypeNames;
+using System.IO;
+using System.Drawing.Imaging;
+
+namespace HeatMap
+{
+    public partial class Form1 : Form
+    {
+        List<Matrix> inputs = new List<Matrix>();
+        List<Matrix> outputs = new List<Matrix>();
+
+        DataSetNoReccurent noReccurent;
+        NNW nnW = new NNW();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        void DataLoad(string MAP)
+        {
+            LoadImages(MAP, inputs);
+
+            GC.Collect();
+            GC.WaitForPendingFinalizers();
+
+            LoadImages(MAP, outputs);
+        }
+
+        private void LoadImages(string directoryPath, List<Matrix> imageList)
+        {
+            string[] paths = Directory.GetFiles(directoryPath);
+            for (int i = 0; i < 20000; i++) // paths.Length
+            {
+                using (Bitmap bitmap = new Bitmap(paths[i]))
+                {
+                    imageList.Add(bitmap.ToMatrix(32, 32));
+                }
+
+                Console.WriteLine(i + " - " + paths.Length);
+            }
+        }
+        void Tr()
+        {
+            GraphCPU graph = new GraphCPU(false);
+            Trainer trainer = new Trainer(graph);
+            trainer.Train(50, 1, 0.0001f, nnW, noReccurent);
+            nnW.Save("nn2.txt");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //nnW = NNW.Load("nn.txt");
+
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+                {
+                    string selectedPath = folderBrowserDialog.SelectedPath;
+
+                    DataLoad(selectedPath);
+
+                    noReccurent = new DataSetNoReccurent(inputs.ToArray(), inputs.ToArray(), new LossMeanSqrSqrt());
+
+                    nnW.AddNewLayer(new Shape(32, 32, 1), new ConvolutionalLayer(new ReLU(0.01), 4) { IsSame = true });
+                    nnW.AddNewLayer(new MaxPooling());
+                    nnW.AddNewLayer(new ConvolutionalLayer(new ReLU(0.01), 8) { IsSame = true });
+                    nnW.AddNewLayer(new MaxPooling());
+                    nnW.AddNewLayer(new ConvolutionalLayer(new ReLU(0.01), 16) { IsSame = true });
+                    nnW.AddNewLayer(new MaxPooling());
+                    nnW.AddNewLayer(new ConvolutionalLayer(new ReLU(0.01), 32) { IsSame = true });
+                    nnW.AddNewLayer(new MaxPooling());
+                    nnW.AddNewLayer(new ConvolutionalLayer(new ReLU(0.01), 64) { IsSame = true });
+                    nnW.AddNewLayer(new UpSampling2DBicubic());
+                    nnW.AddNewLayer(new ConvolutionalLayer(new ReLU(0.01), 32) { IsSame = true });
+                    nnW.AddNewLayer(new UpSampling2DBicubic());
+                    nnW.AddNewLayer(new ConvolutionalLayer(new ReLU(0.01), 16) { IsSame = true });
+                    nnW.AddNewLayer(new UpSampling2DBicubic());
+                    nnW.AddNewLayer(new ConvolutionalLayer(new ReLU(0.01), 8) { IsSame = true });
+                    nnW.AddNewLayer(new UpSampling2DBicubic());
+                    nnW.AddNewLayer(new ConvolutionalLayer(new LinearUnit(), 1) { IsSame = true });
+
+                    Console.WriteLine(nnW);
+
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+
+                    Tr();
+
+                }
+            }
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image Files|*.jpg;*.jpeg;*.png;*.gif";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string selectedFile = openFileDialog.FileName;
+                Bitmap userImages = new Bitmap(selectedFile);
+
+                Matrix outp = nnW.Forward(new NNValue(userImages.ToMatrix(32, 32)), new GraphCPU(false)).ToMatrix();
+
+                Bitmap image1 = ImgConverter.ToBitmap(userImages.ToMatrix(32, 32));
+                Bitmap image2 = ImgConverter.ToBitmap(outp);
+
+                pictureBox1.Image = image1;
+                pictureBox2.Image = GetDifferenceImage(image1, image2);
+            }
+
+        }
+        int hode = 0;
+        private void button1_Click(object sender, EventArgs e)
+        {
+            hode--;
+            if (hode < 0)
+            {
+                hode = 0;
+            }
+            button3.Name = $"Length: {paths.Length}, Hode: {hode}";
+
+            Bitmap userImages = new Bitmap(paths[hode]);
+
+            Matrix outp = nnW.Forward(new NNValue(userImages.ToMatrix(32, 32)), new GraphCPU(false)).ToMatrix();
+
+            Bitmap image1 = ImgConverter.ToBitmap(userImages.ToMatrix(32, 32));
+            Bitmap image2 = ImgConverter.ToBitmap(outp);
+
+            pictureBox1.Image = image1;
+            pictureBox2.Image = GetDifferenceImage(image1, image2);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            hode++;
+            if (hode > paths.Length)
+            {
+                hode = paths.Length;
+            }
+            button3.Name = $"Length: {paths.Length}, Hode: {hode}";
+
+            Bitmap userImages = new Bitmap(paths[hode]);
+
+            Matrix outp = nnW.Forward(new NNValue(userImages.ToMatrix(32, 32)), new GraphCPU(false)).ToMatrix();
+
+            Bitmap image1 = ImgConverter.ToBitmap(userImages.ToMatrix(32, 32));
+            Bitmap image2 = ImgConverter.ToBitmap(outp);
+
+            pictureBox1.Image = image1;
+            pictureBox2.Image = GetDifferenceImage(image1, image2);
+        }
+
+        string[] paths;
+        private void button3_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog folderBrowserDialog = new FolderBrowserDialog())
+            {
+                DialogResult result = folderBrowserDialog.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(folderBrowserDialog.SelectedPath))
+                {
+                    string selectedPath = folderBrowserDialog.SelectedPath;
+
+                    paths = Directory.GetFiles(selectedPath);
+                    ;
+                }
+            }
+        }
+
+
+        public static Bitmap GetDifferenceImage(Bitmap img1, Bitmap img2)
+        {
+            if (img1.Width != img2.Width || img1.Height != img2.Height)
+                throw new ArgumentException("Images must be of the same size");
+
+            Bitmap diffImg = new Bitmap(img1.Width, img1.Height, PixelFormat.Format24bppRgb);
+
+            BitmapData img1Data = img1.LockBits(new Rectangle(0, 0, img1.Width, img1.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            BitmapData img2Data = img2.LockBits(new Rectangle(0, 0, img2.Width, img2.Height), ImageLockMode.ReadOnly, PixelFormat.Format24bppRgb);
+            BitmapData diffImgData = diffImg.LockBits(new Rectangle(0, 0, diffImg.Width, diffImg.Height), ImageLockMode.WriteOnly, PixelFormat.Format24bppRgb);
+
+            int bytesPerPixel = 3;
+            int height = img1.Height;
+            int widthInBytes = img1.Width * bytesPerPixel;
+            byte[] img1Buffer = new byte[img1Data.Stride * img1.Height];
+            byte[] img2Buffer = new byte[img2Data.Stride * img2.Height];
+            byte[] diffBuffer = new byte[diffImgData.Stride * diffImgData.Height];
+
+            System.Runtime.InteropServices.Marshal.Copy(img1Data.Scan0, img1Buffer, 0, img1Buffer.Length);
+            System.Runtime.InteropServices.Marshal.Copy(img2Data.Scan0, img2Buffer, 0, img2Buffer.Length);
+
+            img1.UnlockBits(img1Data);
+            img2.UnlockBits(img2Data);
+
+            for (int y = 0; y < height; y++)
+            {
+                int img1Offset = y * img1Data.Stride;
+                int img2Offset = y * img2Data.Stride;
+                int diffOffset = y * diffImgData.Stride;
+
+                for (int x = 0; x < widthInBytes; x += bytesPerPixel)
+                {
+                    byte bDiff = (byte)Math.Abs(img1Buffer[img1Offset + x] - img2Buffer[img2Offset + x]);
+                    byte gDiff = (byte)Math.Abs(img1Buffer[img1Offset + x + 1] - img2Buffer[img2Offset + x + 1]);
+                    byte rDiff = (byte)Math.Abs(img1Buffer[img1Offset + x + 2] - img2Buffer[img2Offset + x + 2]);
+
+                    diffBuffer[diffOffset + x] = bDiff;
+                    diffBuffer[diffOffset + x + 1] = gDiff;
+                    diffBuffer[diffOffset + x + 2] = rDiff;
+                }
+            }
+
+            System.Runtime.InteropServices.Marshal.Copy(diffBuffer, 0, diffImgData.Scan0, diffBuffer.Length);
+            diffImg.UnlockBits(diffImgData);
+
+            return diffImg;
+        }
+    }
+}
